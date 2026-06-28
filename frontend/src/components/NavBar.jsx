d97d61fda418f3173c7deb83954da911cf639c20
Nipun140905 <gargnipun4@gmail.com>
2026-06-29 01:09:51 +0530

    NavBar Fix

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -46,9 +46,6 @@ const NavBar = () => {
                             </>
                         ) : (
                             <>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/login">Login</Link>
-                                </li>
                                 <li className="nav-item ms-2">
                                     <Link className="btn-nav-primary" to="/login">Get Started</Link>
                                 </li>
